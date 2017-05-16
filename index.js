ce99bc26fdea7680f5e06ada1f0c4398817981ae
Andrej <sperling@protonmail.com>
2017-05-16 21:31:39 +0200

    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(8080, function () {
+  console.log('Server is listening on port 8080')
+});

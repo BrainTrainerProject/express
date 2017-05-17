3fca0198d9dd48b8e13f58288b6de0ac8d556d8c
Andrej <sperling@protonmail.com>
2017-05-17 09:19:26 +0200

    Build von cyclejs eingebunden

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const frontend = require('bt-cyclejs');	// name vom module wie er im package steht
+const path = require('path');
 
 var app = express();
 
 app.use(express.static('public'));
+
+// 	Über buildWebApp() soll später der Public Ordner mit Inhalt generiert werden
+//	kann jetzt theoretisch genutzt werden, 
+//	ab hier ist cyclejs repo für den build zuständig
+//
+// 	folder 'cwd + ./public_custom_folder'
+const examplePath = path.resolve(process.cwd(), './public_custom_folder');
+frontend.buildWebApp(examplePath)
 
 app.listen(8080, function () {
   console.log('Server is listening on port 8080')

24c39adf9c902c6189b4571b02269b5f788d3267
Unknown <christian_philipp.sassmann@fh-bielefeld.de>
2017-07-25 20:56:30 +0200

    Rest Test v.0.5 Notecard CRUD complete

--- src/test/controllers/notecard.controller.test.js
+++ src/test/controllers/notecard.controller.test.js
@@ -8,6 +8,9 @@ chai.use(chaiHttp);
 const TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MDA4MTQzODQsImV4cCI6MTUzMjM1MDM4NCwiYXVkIjoiZXE5THZGdU1lazBENGJTY0lzT2JKSjFiNjFCdUtJQjMiLCJpc3MiOiJodHRwczovL2JyYWludHJhaW5lci5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NTk3NDliN2EwOTBmMDQwNWY4MzNiN2FiIn0.iWp27qQCP-y94UrhqfPV-eKX84A0MJlkNypjY-XkbaA';
 
 mocha.describe('Notecards REST', () => {
+  /*
+  READ
+  */
   mocha.it('it should GET all notecards', (done) => {
     chai.request(app)
     .get('/api/notecard')
@@ -19,7 +22,10 @@ mocha.describe('Notecards REST', () => {
     });
   }).timeout(30000);
 
-/*  mocha.it('it should POST a notecard', (done) => {
+  /*
+  CREATE
+  */
+  mocha.it('it should POST a notecard', (done) => {
     const notecard = {
       title: 'Englisch Vokabeln',
       task: 'Was heißt comment?',
@@ -31,7 +37,6 @@ mocha.describe('Notecards REST', () => {
     .send(notecard)
     .end((err, res) => {
       chai.expect(res).to.have.status(200);
-      console.log(res.body);
       chai.expect(res.body).to.be.a('object');
       chai.expect(res.body).to.have.property('task');
       chai.expect(res.body).to.have.property('answer');
@@ -39,18 +44,49 @@ mocha.describe('Notecards REST', () => {
     });
   }).timeout(30000);
 
-  mocha.it('it should DELETE all notecards', (done) => {
+  /*
+  UPDATE
+  */
+  mocha.it('it should UPDATE a notecard', (done) => {
+    const notecard = {
+      title: 'Englisch Vokabeln Update',
+      task: 'Was heißt comment?',
+      answer: 'Kommentar',
+    };
+    chai.request(app)
+    .get('api/notecard')
+    .set('Authorization', TOKEN)
+    .end((err, res) => {
+      chai.expect(res).to.have.status(200);
+      /* eslint no-underscore-dangle: 0 */
+      const updid = (res.body)[0]._id;
+      const updurl = `/api/notecard/${updid}`;
+      chai.request(app)
+      .put(updurl)
+      .set('Authorization', TOKEN)
+      .end((err2, res2) => {
+        if (err2) { console.log(err2); }
+        chai.expect(res2).to.have.status(200);
+        chai.expect(res2.body.property('title')).to.equal(notecard.title);
+        done();
+      });
+    });
+  }).timeout(30000);
+
+  /*
+  DELETE
+  */
+  mocha.it('it should DELETE a notecard', (done) => {
     chai.request(app)
     .get('/api/notecard')
     .set('Authorization', TOKEN)
     .end((err, res) => {
-      console.log(res.body);
       chai.expect(res).to.have.status(200);
       /* eslint no-underscore-dangle: 0 */
-    /*  const delid = (res.body)[0]._id;
-      console.log(delid);
+      const delid = (res.body)[0]._id;
+      const delurl = `/api/notecard/${delid}`;
       chai.request(app)
-      .del('/api/notecard/', delid)
+      .del(delurl)
       .set('Authorization', TOKEN)
       .end((err2, res2) => {
         if (err2) { console.log(err2); }
@@ -59,5 +95,5 @@ mocha.describe('Notecards REST', () => {
         done();
       });
     });
-  }).timeout(30000);*/
+  }).timeout(30000);
 });
